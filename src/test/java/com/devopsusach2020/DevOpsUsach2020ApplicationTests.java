e3168313a103b2da4e9e8a5ab5b413055076c903
Daniel Gavilan <dannylnx@gmail.com>
2022-01-17 20:17:10 -0300

    Se agregan las correcciones

--- src/test/java/com/devopsusach2020/DevOpsUsach2020ApplicationTests.java
+++ src/test/java/com/devopsusach2020/DevOpsUsach2020ApplicationTests.java
@@ -2,12 +2,17 @@ package com.devopsusach2020;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import com.devopsusach2020.model.Pais;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class DevOpsUsach2020ApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void test1() {
+		Pais p = new Pais();
+    	assertNotNull(p);
+
 	}
 
 }
